a42d1225e741c8449dcf756b7b289f4ee5eedcfc
Dima_Fedorov <fedorov.d.edu@gmail.com>
2022-11-15 00:10:50 +0500

    Hello World!

---
+++ AAU22_1_fedorov_d/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AAU22_1_fedorov_d
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
